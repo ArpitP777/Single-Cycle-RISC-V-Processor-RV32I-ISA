6c07b5611b0ee90f7c0f8000fd6cf3e70b8cb4ac
Arpit Prakash <thegreatkingarpit@gmail.com>
2026-08-27 03:13:02 +0530

    control unit and pc

---
+++ control_unit.v
@@ -0,0 +1,12 @@
+module cu(
+    input funct7,
+    input [2:0] funct3,
+    input [6:0] opcode,
+    output pc_sel,
+    output result_sel,
+    output wr_mem,
+    output [2:0] alu_ctrl,
+    output alu_sel,
+    output imm_sel,
+    output wr_reg,
+);
--- instDecoder.v
+++
@@ -1,3 +0,0 @@
-module instDecoder();
-    
-endmodule
