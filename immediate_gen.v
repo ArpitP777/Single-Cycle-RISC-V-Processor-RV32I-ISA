562b87919e5625e73c7f4f27692dbd9597651a80
Arpit Prakash <thegreatkingarpit@gmail.com>
2026-08-28 23:15:46 +0530

    fixed J type instr indexing

--- immediate_gen.v
+++ immediate_gen.v
@@ -8,8 +8,8 @@ module imm_gen(
         case(imm_sel) 
             2'b00: imm_out = {{20{instr[31]}}, instr[31:20]};       // I 
             2'b01: imm_out = {{20{instr[31]}}, instr[31:25], instr[11:7]};      // S
-            2'b10: imm_out = {19{instr[31]}, instr[31], instr[7], instr[30:25], instr[11:8], 1'b0};      // B
-            2'b11: imm_out = {11{instr[31]}, instr[31], instr[19:12], instr[20], instr[30:21], 1'b0};       // J
+            2'b10: imm_out = {{19{instr[31]}}, instr[31], instr[7], instr[30:25], instr[11:8], 1'b0};      // B
+            2'b11: imm_out = {{11{instr[31]}}, instr[31], instr[19:12], instr[20], instr[30:21], 1'b0};       // J
         endcase
     end
 
