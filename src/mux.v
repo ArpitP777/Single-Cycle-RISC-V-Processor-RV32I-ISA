1ade0993ea4c583ecf2f2d4187ee71ec5fab0321
Arpit Prakash <thegreatkingarpit@gmail.com>
2026-09-03 23:04:11 +0530

    pc and alu mux implemented

--- src/mux.v
+++ src/mux.v
@@ -1,8 +1,25 @@
 module pc_inc(
+    input pc_sel,
     input [32:0] pc,
-    output reg [32:0] pc_plus4
+    input [32:0] imm_out,
+    output reg [32:0] pc_next
 );
 
-    assign pc_plus4 = pc + 4;
+    always@(*) begin
+        pc_next = pc_sel?(pc + 4):(pc + imm_out);
+    end
+endmodule
+
+module alu_in(
+    input alu_sel,
+    input [32:0] rd2,
+    input [32:0] imm_out,
+    output reg [32:0] in_alu
+);
+
+    always@(*) begin
+        in_alu <= alu_sel?(imm_out):(rd2);
+    end
 
 endmodule
+
